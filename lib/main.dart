a3b01ed347d497a418451e47849d23520381f66a
SlowedWalk <idetoratojo1@gmail.com>
2023-11-19 22:06:22 +0100

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
+void main() => runApp(
+  const ProviderScope(
+    child: App()
+  )
+);
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator + (T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class CounterNotifier extends StateNotifier<int?> {
+  CounterNotifier(): super(null);
+
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<CounterNotifier, int?>((ref) {
+  return CounterNotifier();
+});
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -12,9 +35,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
@@ -23,25 +43,41 @@ class App extends StatelessWidget {
   }
 }
 
-final currentDateProvider = Provider<DateTime>((ref) => DateTime.now());
-
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final dateTime = ref.watch(currentDateProvider);
-    log(dateTime.toString());
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hooks Riverpod'),
+        title: Consumer(
+          builder: (context, ref, child) {
+            final count = ref.watch(counterProvider);
+            final text = count == null
+              ? 'Press the button'
+              : count.toString();
+              return Text(text);
+          },
+          child: const Text('Home Page')
+        ),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          dateTime.toIso8601String(),
-          style: Theme.of(context).textTheme.headlineMedium
-        )
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton(
+              onPressed: ref.read(counterProvider.notifier).increment,
+              child: const Text(
+                'Increment counter',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
